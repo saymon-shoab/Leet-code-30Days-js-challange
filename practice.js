ff56ccd166db2da838bd9dd3458de6872512b113
Saymon Shoab <saymon@Saymons-MacBook-Air.local>
2024-06-20 08:20:32 +0600

    problem 6 resolved

--- practice.js
+++ practice.js
@@ -1,21 +1,30 @@
 
-const arr = [0,10,20,30]
+// const arr = [0,10,20,30]
 
-var filter = function(arr, fn) {
-   console.log(arr)
+// var filter = function(arr, fn) {
+//    console.log(arr)
 
-   const result = []
-   for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i],i)) {
-       result.push(arr[i])
-    } 
-   }
-   return "result"
+//    const result = []
+//    for (let i = 0; i < arr.length; i++) {
+//     if (fn(arr[i],i)) {
+//        result.push(arr[i])
+//     } 
+//    }
+//    return 
     
-};
+// };
 
 // function greaterThen10(n,i){
 //     if(i>n) return true
 // }
 
-console.log(filter(arr))
+// console.log(filter(arr))
+
+var reduce = function(nums, fn, init) {
+    const res = init
+    for (let i = 0; i < nums.length; i++) {
+        res = fn(res,arr[i])
+        
+    }
+    return res
+};
