f5796e1e562e5dfe946aa5ccfc49dec5848cc805
Saymon Shoab <saymon@Saymons-MacBook-Air.local>
2024-06-22 10:08:23 +0600

    problem 9 solved

--- practice.js
+++ practice.js
@@ -1,1 +1,26 @@
 
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+  return function(...args){
+      var exicutionCount = 0;
+      console.log(exicutionCount)
+      if (exicutionCount===0) {
+         exicutionCount++
+         return fn(...args)
+        }else{
+          return undefined
+        }
+      }
+  }
+
+ let fn = (a,b,c) => (a + b + c)
+ let onceFn = once(fn)
+
+console.log(onceFn(1,2,3))  // 6
+console.log(onceFn(2,3,6))   // returns undefined without calling fn
+console.log(onceFn(2,3,8))   // returns undefined without calling fn
+
