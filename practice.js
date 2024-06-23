42d4fedff0c0c8337177f5f8c1b94124306539c2
Saymon Shoab <saymon@Saymons-MacBook-Air.local>
2024-06-23 07:27:21 +0600

    problem 14 solved

--- practice.js
+++ practice.js
@@ -5,36 +5,39 @@
  * @return {Function}
  */
 var cancellable = function(fn, args, t) {
-    const cancelFn = function (){
-      clearTimeout(timer)
-    }
-
-    const timer = setTimeout(()=>{
+   fn(...args);
+   let timer = setInterval(()=>{
       fn(...args)
-    },t)
-    return cancelFn
+   },t)
+   let cancelFn = () => clearInterval(timer);
+   return cancelFn
 };
 
-
-   const result = [];
- 
-   const fn = (x) => x * 5;
-   const args = [2], t = 20, cancelTimeMs = 50;
- 
-   const start = performance.now();
- 
-   const log = (...argsArr) => {
-       const diff = Math.floor(performance.now() - start);
-       result.push({"time": diff, "returned": fn(...argsArr)});
-   }
-        
-   const cancel = cancellable(log, args, t);
- 
-   const maxT = Math.max(t, cancelTimeMs);
-            
-   setTimeout(cancel, cancelTimeMs);
- 
-   setTimeout(() => {
-       console.log(result); // [{"time":20,"returned":10}]
-   }, maxT + 15)
- 
+/**
+*  const result = [];
+*
+*  const fn = (x) => x * 2;
+*  const args = [4], t = 35, cancelTimeMs = 190;
+*
+*  const start = performance.now();
+*
+*  const log = (...argsArr) => {
+*      const diff = Math.floor(performance.now() - start);
+*      result.push({"time": diff, "returned": fn(...argsArr)});
+*  }
+*       
+*  const cancel = cancellable(log, args, t);
+*
+*  setTimeout(cancel, cancelTimeMs);
+*   
+*  setTimeout(() => {
+*      console.log(result); // [
+*                           //     {"time":0,"returned":8},
+*                           //     {"time":35,"returned":8},
+*                           //     {"time":70,"returned":8},
+*                           //     {"time":105,"returned":8},
+*                           //     {"time":140,"returned":8},
+*                           //     {"time":175,"returned":8}
+*                           // ]
+*  }, cancelTimeMs + t + 15)    
+*/
